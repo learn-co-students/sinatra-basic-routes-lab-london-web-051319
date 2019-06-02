cdb8ae0af7b9f00e2edabbfc7d8ceecb85e92129
moniet <moniet.sawhney@gmail.com>
2019-06-02 14:07:21 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Bak Lau"
+  end
+
+  get '/hometown' do
+    "My hometown is America"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Breathe"
+  end
 end
