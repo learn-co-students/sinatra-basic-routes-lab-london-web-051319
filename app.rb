0c917c7235fb73c7a510d06b2fbde82eb7f9771c
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-02 03:18:06 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        resp.status 200
+        resp.write "My name is Vlad"
+    end
+
+    get '/hometown' do
+        resp.status 200
+        resp.write "My hometown is Ruse"
+    end
+
+    get '/favorite-song' do
+        resp.status 200
+        resp.write "My favorite song is Saturnalia"
+    end
+
 end
