c3dc32c4b81aee0d675544a1115c05719c18f1f6
chrisdews <cldews1@gmail.com>
2019-06-03 11:17:59 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+  		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Chris"
+	end
+
+	get	'/hometown' do
+		"My hometown is Wilmslow"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is a nice song"
+	end
+
+
+
 end
